827bae5b4bd511be89d094cd97949af80d612e81
n3rmal <nirmaldhakal2032@gmail.com>
2023-10-13 20:07:08 +0545

    data abstraction added

---
+++ oop/data_abstraction/Planet.h
@@ -0,0 +1,24 @@
+// Write/move your code here
+#ifndef PLANET_H
+#define PLANET_H
+
+class Planet {
+  double radius;
+  double mass;
+  const double G = 6.673e-11;
+  
+  public:
+  Planet(double r, double m);
+  
+  double getMass();
+  
+  void setMass(double m);
+  
+  double getRadius();
+  
+  void setRadius(double r);
+  
+  double calculateGravity();
+};
+
+#endif
